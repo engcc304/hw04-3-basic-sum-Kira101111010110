0d91f59cba525e9af68c888634336821a284bdce
Kira101111010110 <137255457+Kira101111010110@users.noreply.github.com>
2023-07-27 15:23:00 +0700

    p

--- Homework/HW04/hw04-3-basic-operator.cpp
+++ Homework/HW04/hw04-3-basic-operator.cpp
@@ -13,3 +13,10 @@
         Answer = 6
 */
 #include <stdio.h>
+int main() {
+    int a, b, c ;
+    printf( "Enter 3 numbers =  " ) ;
+    scanf( "%d %d %d", &a, &b, &c ) ;
+    printf( "Answer = %d", a + b + c ) ;
+    return 0 ;
+}
